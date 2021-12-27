fe92c1dc721662cbd815ba87fa9a2c367c50599e
Jorge de Carvalho <jorgercj@dcomp.ufs.br>
2021-12-27 20:35:53 -0300

    Using waitUntil to add unsync test with no return

--- TestsDemo/AsyncProcess.swift
+++ TestsDemo/AsyncProcess.swift
@@ -10,6 +10,12 @@ import UIKit
 class AsyncProcess {
     var name = ""
     
+    func doStuff(_ completion: @escaping (String) -> ()) {
+        DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 0.1) { [weak self] in
+            completion("TestsDemo")
+        }
+    }
+    
     func callThatResultsInSideEffect() {
         DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 0.1) { [weak self] in
             self?.name = "TestsDemo"
--- TestsDemoTests/AsyncProcessTests.swift
+++ TestsDemoTests/AsyncProcessTests.swift
@@ -13,11 +13,22 @@ class AsyncProcessTests: QuickSpec {
     override func spec() {
         describe("Acync Process") {
             context("callThatResultsInSideEffect") {
-                it("Test") {
+                it("Test using toEventually") {
                     let asyncProcess = AsyncProcess()
                     asyncProcess.callThatResultsInSideEffect()
-                    //toEventually is used in async tests and process.
+                    // toEventually is used in async tests and process.
                     expect(asyncProcess.name).toEventually(equal("TestsDemo"))
+                }
+                
+                it("Do Stuff test using waitUntil") {
+                    let asyncProcess = AsyncProcess()
+                    // waitUntil is used to async tests that have no return
+                    waitUntil { done in
+                        asyncProcess.doStuff { result in
+                            expect(result).to(equal("TestsDemo"))
+                            done()
+                        }
+                    }
                 }
             }
         }
